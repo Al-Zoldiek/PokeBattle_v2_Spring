ec71559d2cdbc766d7ce334eb4dfbe44cdc6b964
AD\arebiere2018 <alexis.rebiere2018@campus-eni.fr>
2021-11-10 11:16:13 +0100

    1st commit

---
+++ src/main/java/fr/eni/ecole/pokeBattle/PokeBattleApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.pokeBattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeBattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeBattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/ecole/pokeBattle/PokeBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.ecole.pokeBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeBattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
